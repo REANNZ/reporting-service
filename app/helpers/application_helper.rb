68e7cdfeba751bac59f99c7cd3e11f5bb1ece26b
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2020-11-05 14:21:14 +1300

    Version: 1.3.2-tuakiri4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.3.2-tuakiri3'
+  VERSION = '1.3.2-tuakiri4'
 
   def permitted?(action)
     @subject.try(:permits?, action)
