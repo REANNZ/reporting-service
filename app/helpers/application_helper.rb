b5cb3aa315281c1b32ffde68adc746ed34a12ecf
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2023-03-14 12:08:05 +1300

    Version: 1.4.7-tuakiri6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.4.7-tuakiri5'
+  VERSION = '1.4.7-tuakiri6'
 
   # rubocop:disable Rails/HelperInstanceVariable
   def permitted?(action)
