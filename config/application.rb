421ac94a85cc49c44e98a7e544378d34fa125672
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2026-03-24 18:13:10 +1300

    fix: rails 7.2: change use of load_defaults
    Existing code in config/application.rb:
    
        config.load_defaults = 6.1
    
    results into
    
        Cannot assign to `load_defaults`, it is a configuration method (NoMethodError)
    
    Fix by changing to:
    
        config.load_defaults 6.1

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 module ReportingService
   class Application < Rails::Application
-    config.load_defaults = 6.1
+    config.load_defaults 6.1
 
     config.autoload_paths += [
       File.join(config.root, 'lib'),
